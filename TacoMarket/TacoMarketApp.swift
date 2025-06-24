0692e46558659798884c21b166f9fb6838541e2d
LRCorvalan <lrcorvalan@gmail.com>
2025-06-24 18:03:25 -0500

    Initial Commit

---
+++ TacoMarket/TacoMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  TacoMarketApp.swift
+//  TacoMarket
+//
+//  Created by Luis Corvalan on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct TacoMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
