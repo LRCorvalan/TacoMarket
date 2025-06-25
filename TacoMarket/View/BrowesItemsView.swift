781b58846ece62d617bd649268c329e1c5b00a47
LRCorvalan <lrcorvalan@gmail.com>
2025-06-24 19:50:29 -0500

    Login

---
+++ TacoMarket/View/BrowesItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  BrowesItemsView.swift
+//  TacoMarket
+//
+//  Created by Luis Corvalan on 6/24/25.
+//
+
+import SwiftUI
+
+struct BrowesItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BrowesItemsView()
+}
---
+++ TacoMarket/View/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  TacoMarket
+//
+//  Created by Luis Corvalan on 6/24/25.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
