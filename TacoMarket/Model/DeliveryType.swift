7b192a19cf49eda37567779efd4cb598914c1b3e
LRCorvalan <lrcorvalan@gmail.com>
2025-07-08 19:06:07 -0500

    Connected SwiftData. Time to user it.

---
+++ TacoMarket/Model/DeliveryType.swift
@@ -0,0 +1,12 @@
+//
+//  Delivery.swift
+//  TacoMarket
+//
+//  Created by Luis Corvalan on 6/24/25.
+//
+
+import Foundation
+
+enum DeliveryType: String, Codable {
+    case standard, oneDay, twoDay
+}
